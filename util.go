aec1fe075077a61d2425c11533824011b760e1d7
JG <julioguillermo0802@gmail.com>
2021-10-25 10:25:38 -0400

    starting...

--- util.go
+++ util.go
@@ -1,63 +1,63 @@
 package neat
 
 import (
-	"fmt"
-	"math/rand"
+    "math/rand"
+    "fmt"
 )
 
 // Check loops
-func checkLoops(neuron, from INeuron) bool {
-	if neuron == from {
-		return true
-	}
-	for i := 0; i < len(from.Connections); i++ {
-		if checkLoops(neuron, from.Connections[i]) {
-			return true
-		}
-	}
-	return false
+func checkLoops(neuron, from *Neuron) bool {
+    if neuron == from {
+        return true
+    }
+    for i := 0; i < len(from.Connections); i ++ {
+        if checkLoops(neuron, from.Connections[i]) {
+            return true
+        }
+    }
+    return false
 }
 
 // Random
 func random() float64 {
-	return rand.Float64()
+    return rand.Float64()
 }
 
 func randWeight() float64 {
-	return rand.Float64()*2 - 1
+    return rand.Float64() * 2 - 1
 }
 
 func randScaleWeight(scale float64) float64 {
-	return randWeight() * scale
+    return randWeight() * scale
 }
 
 func probability(pro float64) bool {
-	return rand.Float64() < pro
+    return rand.Float64() < pro
 }
 
 func randIndex(from, to int) int {
-	return rand.Intn(to-from) + from
+    return rand.Intn(to - from) + from
 }
 
 func randInt(max int) int {
-	return rand.Intn(max)
+    return rand.Intn(max)
 }
 
 // Neurons name
 func inputName(i int) string {
-	return "I-" + fmt.Sprint(i)
+    return "I-" + fmt.Sprint(i)
 }
 
 func outputName(i int) string {
-	return "O-" + fmt.Sprint(i)
+    return "O-" + fmt.Sprint(i)
 }
 
 // Array
-func getIndex(neuron INeuron, neurons []INeuron) int {
-	for i := 0; i < len(neurons); i++ {
-		if neuron == neurons[i] {
-			return i
-		}
-	}
-	return -1
+func getIndex(neuron *Neuron, neurons []*Neuron) int {
+    for i := 0; i < len(neurons); i ++ {
+        if neuron == neurons[i] {
+            return i
+        }
+    }
+    return -1
 }
