820c6c2940647890fc5034fa39c2ba2ea8b3cfeb
JG <julioguillermo0802@gmail.com>
2021-10-30 15:45:09 -0400

    Null neurons connections fixed...

--- individual.go
+++ individual.go
@@ -1,125 +1,127 @@
 package neat
 
 type Individual struct {
-    InputSize       int
-    OutputSize      int
+	InputSize  int
+	OutputSize int
 
-    Fitness         float64
+	Fitness float64
 
-    Neurons         []*Neuron
+	Neurons []*Neuron
 }
 
 func MakeIndividual(InputSize, OutputSize int) *Individual {
-    neurons := []*Neuron{}
+	neurons := []*Neuron{}
 
-    // Make input neurons
-    // They are the firsts neurons in the neurons array
-    for i := 0; i < InputSize; i ++ {
-        neurons = append(neurons, MakeNamedNeuron(inputName(i)))
-    }
-    // Make output neurons
-    // They are the firsts neurons after the inputs neurons in the neurons array
-    for i := 0; i < OutputSize; i ++ {
-        neurons = append(neurons, MakeNamedNeuron(outputName(i)))
-    }
+	// Make input neurons
+	// They are the firsts neurons in the neurons array
+	for i := 0; i < InputSize; i++ {
+		neurons = append(neurons, MakeNamedNeuron(inputName(i)))
+	}
+	// Make output neurons
+	// They are the firsts neurons after the inputs neurons in the neurons array
+	for i := 0; i < OutputSize; i++ {
+		neurons = append(neurons, MakeNamedNeuron(outputName(i)))
+	}
 
-    return &Individual {
-        InputSize:      InputSize,
-        OutputSize:     OutputSize,
+	return &Individual{
+		InputSize:  InputSize,
+		OutputSize: OutputSize,
 
-        Fitness:        0,
+		Fitness: 0,
 
-        Neurons:        neurons,
-    }
+		Neurons: neurons,
+	}
 }
 
 func (self *Individual) Output(activation Activation, input []float64) []float64 {
-    // Put the inputs into the input neurons
-    for i := 0; i < self.InputSize; i ++ {
-        self.Neurons[i].SetOutput(input[i])
-    }
-    // Reset the others neurons
-    for i := self.InputSize; i < len(self.Neurons); i ++ {
-        self.Neurons[i].Reset()
-    }
+	// Put the inputs into the input neurons
+	for i := 0; i < self.InputSize; i++ {
+		self.Neurons[i].SetOutput(input[i])
+	}
+	// Reset the others neurons
+	for i := self.InputSize; i < len(self.Neurons); i++ {
+		self.Neurons[i].Reset()
+	}
 
-    // Get the output
-    output := make([]float64, self.OutputSize)
-    for i := self.InputSize; i < self.InputSize + self.OutputSize; i ++ {
-        output[i - self.InputSize] = self.Neurons[i].Output(activation)
-    }
-    return output
+	// Get the output
+	output := make([]float64, self.OutputSize)
+	for i := self.InputSize; i < self.InputSize+self.OutputSize; i++ {
+		output[i-self.InputSize] = self.Neurons[i].Output(activation)
+	}
+	return output
 }
 
 func (self *Individual) Mutate(newNeuronRate, changeBiasRate, mutSize float64) {
-    // Select any neuron to mutate except the inputs neurons
-    neuron_to_mutate := self.Neurons[randIndex(self.InputSize, len(self.Neurons))]
-    if probability(changeBiasRate) && neuron_to_mutate.HasConnections() {
-        neuron_to_mutate.MutateBias(mutSize)
-    } else {
-        // Select any neuron to connect except the outputs neurons
-        neuron := self.Neurons[(randIndex(self.OutputSize, len(self.Neurons)) + self.InputSize) % len(self.Neurons)]
-        // "neuron_to_mutate" will has a connection to "neuron"
-        // to prevent a connections loop the selected "neuron" could not has a connection to "neuron_to_mutate"
-        for checkLoops(neuron_to_mutate, neuron) {
-            neuron = self.Neurons[(randIndex(self.OutputSize, len(self.Neurons)) + self.InputSize) % len(self.Neurons)]
-        }
+	// Select any neuron to mutate except the inputs neurons
+	neuron_to_mutate := self.Neurons[randIndex(self.InputSize, len(self.Neurons))]
+	if probability(changeBiasRate) && neuron_to_mutate.HasConnections() {
+		neuron_to_mutate.MutateBias(mutSize)
+	} else {
+		// Select any neuron to connect except the outputs neurons
+		neuron := self.Neurons[(randIndex(self.OutputSize, len(self.Neurons))+self.InputSize)%len(self.Neurons)]
+		// "neuron_to_mutate" will has a connection to "neuron"
+		// to prevent a connections loop the selected "neuron" could not has a connection to "neuron_to_mutate"
+		for checkLoops(neuron_to_mutate, neuron) {
+			neuron = self.Neurons[(randIndex(self.OutputSize, len(self.Neurons))+self.InputSize)%len(self.Neurons)]
+		}
 
-        // Mutate the selected neuron to the other one
-        newNeuron := neuron_to_mutate.Mutate(neuron, newNeuronRate, mutSize)
-        // if the newNeuron is not nil the mutation create a new neuron between the other two
-        if newNeuron != nil {
-            self.Neurons = append(self.Neurons, newNeuron)
-        }
-    }
+		// Mutate the selected neuron to the other one
+		newNeuron := neuron_to_mutate.Mutate(neuron, newNeuronRate, mutSize)
+		// if the newNeuron is not nil the mutation create a new neuron between the other two
+		if newNeuron != nil {
+			self.Neurons = append(self.Neurons, newNeuron)
+		}
+	}
 }
 
 // Fitness
 func (self *Individual) GetFitness() float64 {
-    return self.Fitness
+	return self.Fitness
 }
 
 func (self *Individual) SetFitness(fitness float64) {
-    self.Fitness = fitness
+	self.Fitness = fitness
 }
 
 func (self *Individual) AddFitness(fitness float64) {
-    self.Fitness += fitness
+	self.Fitness += fitness
 }
 
 // Serialization
 type JsonIndividual struct {
-    Neurons     []JsonNeuron    `json:"neurons"`
+	Fitness float64      `json:"fitness"`
+	Neurons []JsonNeuron `json:"neurons"`
 }
 
 func MakeIndividualFromJson(jsonIndividual JsonIndividual, inputSize, outputSize int) *Individual {
-    neuronsLen := len(jsonIndividual.Neurons)
-    neurons := make([]*Neuron, neuronsLen)
+	neuronsLen := len(jsonIndividual.Neurons)
+	neurons := make([]*Neuron, neuronsLen)
 
-    for i := 0; i < neuronsLen; i ++ {
-        neurons[i] = MakeNeuronFromJson(jsonIndividual.Neurons[i])
-    }
-    for i := 0; i < neuronsLen; i ++ {
-        neurons[i].SetConnectionsFromIndex(neurons, jsonIndividual.Neurons[i].Connections)
-    }
+	for i := 0; i < neuronsLen; i++ {
+		neurons[i] = MakeNeuronFromJson(jsonIndividual.Neurons[i])
+	}
+	for i := 0; i < neuronsLen; i++ {
+		neurons[i].SetConnectionsFromIndex(neurons, jsonIndividual.Neurons[i].Connections)
+	}
 
-    return &Individual {
-        InputSize:      inputSize,
-        OutputSize:     outputSize,
-        Fitness:        0,
-        Neurons:        neurons,
-    }
+	return &Individual{
+		InputSize:  inputSize,
+		OutputSize: outputSize,
+		Fitness:    jsonIndividual.Fitness,
+		Neurons:    neurons,
+	}
 }
 
 func (self *Individual) GetJsonIndividual() JsonIndividual {
-    neuronsLen := len(self.Neurons)
-    neurons := make([]JsonNeuron, neuronsLen)
+	neuronsLen := len(self.Neurons)
+	neurons := make([]JsonNeuron, neuronsLen)
 
-    for i := 0; i < neuronsLen; i ++ {
-        neurons[i] = self.Neurons[i].GetJsonNeuron(self.Neurons)
-    }
+	for i := 0; i < neuronsLen; i++ {
+		neurons[i] = self.Neurons[i].GetJsonNeuron(self.Neurons)
+	}
 
-    return JsonIndividual {
-        Neurons:    neurons,
-    }
+	return JsonIndividual{
+		Fitness: self.Fitness,
+		Neurons: neurons,
+	}
 }
